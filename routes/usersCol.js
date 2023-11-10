e4453d527152ca5c05752518b77483214296e9c1
JuanSosa17 <juan.sosav17@gmail.com>
2023-11-09 21:13:15 -0300

    9/11

--- routes/usersCol.js
+++ routes/usersCol.js
@@ -5,10 +5,19 @@ const controller = require('../controllers/users');
 router.get('/', async (req, res) => {
     const pageSize = req.query.pageSize ? parseInt(req.query.pageSize): 0;
     const page = req.query.page ? parseInt(req.query.page): 0;
-
+    console.log("prueba")
     res.json(await controller.getAllUsers(pageSize, page));
 });
 
-
+router.post('/register/', async(req, res) => {
+    try {
+      const newUser = req.body;
+      const result = await controller.addUser(newUser);
+      res.send(result);
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error);
+    }
+  });
 
 module.exports = router;
