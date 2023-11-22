dd9f2e576529c075467e41e8d03d61f72eaa24a8
Mariam SLeiman <msleiman@santandertecnologia.com.ar>
2023-11-22 15:57:21 -0300

    route reservas por cliente, reserva por id, delete reserva si la hizo el cliente

--- routes/bookings.js
+++ routes/bookings.js
@@ -5,24 +5,36 @@ const { auth } = require('../middleware/auth');
 const jwtDecode = require("jwt-decode");
 
 router.delete('/delete/:id', auth, async (req, res) => {
-
   try {
     const jwtInfo = jwtDecode(req.header("Token"));
     const user_id = jwtInfo._id;
-    const myBooking = req.body;
-    const result = await controller.findBookingForCreateId(user_id);
-    if (result.acknowledged) {
-      const result = await controller.deleteBooking(myBooking);
-      res.send(result);
+    const resultForId = await controller.findBookingForId(req.params.id);
+    if (resultForId.user_id === user_id) {
+      await controller.deleteBooking(req.params.id);
+      res.render('index', { title: 'BORRADO' });
     } else {
-      res.status(400).send(error);
+      console.log('else');
     }
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
   }
+});
 
-  //res.send(await controller.deleteBooking(req.params.id));
+router.get('/find/:id', async (req, res) => {
+  res.json(await controller.findBookingForId(req.params.id));
+});
+
+router.get('/find-for-user', auth, async (req, res) => {
+  try {
+    const jwtInfo = jwtDecode(req.header("Token"));
+    const user_id = jwtInfo._id;
+    const result = await controller.findBookingForCreateId(user_id);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 });
 
 router.get('/', async (req, res) => {
